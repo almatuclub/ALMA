7fb56ad9eaa5f7011aa8125ed3ebd07723aa91bc
almatuclub <almatuclub@gmail.com>
2026-04-07 17:58:22 -0300

    Delete ide directory

--- ide/db.js
+++
@@ -1,8 +0,0 @@
-// db.js
-// Supabase Configuration
-const SUPABASE_URL = 'https://iuhnhexotyrnflsmpzxi.supabase.co';
-const SUPABASE_KEY = 'sb_publishable_DwIRv7GbWFpeBXyDeghA1g_sfXSoyTp';
-
-// Initialize Supabase Client
-// This requires the Supabase CDN script to be loaded before this file
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
